5ab4fbe489ca3b95287382ff2d109e5b8f092e47
ksikk <hgs0219@gachon.ac.kr>
2022-07-22 18:14:44 +0900

    yet

--- app/src/main/java/com/gachonumc/threejeon/LoginActivity.kt
+++ app/src/main/java/com/gachonumc/threejeon/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.gachonumc.threejeon
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.gachonumc.threejeon.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState:Bundle?
+    ): View? {
+        _binding = ActivityLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.makeAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_LoginActivity_to_MakeaccountActivity)
+        }
+
+
     }
 }
