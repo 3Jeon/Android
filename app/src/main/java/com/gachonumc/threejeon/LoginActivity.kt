f95c3b04629b3535b52c0b88040036d7b87a9b86
smpark0213 <smpark0213@naver.com>
2022-07-27 15:15:28 +0900

    move make gogo

--- app/src/main/java/com/gachonumc/threejeon/LoginActivity.kt
+++ app/src/main/java/com/gachonumc/threejeon/LoginActivity.kt
@@ -1,13 +1,37 @@
 package com.gachonumc.threejeon
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gachonumc.threejeon.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //회원가입
+        binding.makeAccount.setOnClickListener {
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+        //id 찾기
+        binding.loginSearchId.setOnClickListener {
+            startActivity(Intent(this, FindIdActivity::class.java))
+        }
+
+        //pw 찾기
+        binding.loginSearchPw.setOnClickListener {
+            startActivity(Intent(this, FindPwActivity::class.java))
+        }
+
+        //로그인 확인
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
 
 
     }
---
+++ app/src/main/java/com/gachonumc/threejeon/interface/RetrofitService.kt
@@ -0,0 +1,15 @@
+package com.gachonumc.threejeon.`interface`
+
+import okhttp3.Call
+import retrofit2.http.Field
+import retrofit2.http.POST
+
+interface RetrofitService {
+/*
+    @POST("/test")
+    fun postRequest(
+        @Field("store") store: String
+    ): Call<ResponseDTO>
+    */
+ 
+}
