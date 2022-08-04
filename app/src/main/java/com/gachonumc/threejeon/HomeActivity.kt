b270dd1beec3efbe11673d676cc153a566b7f2ee
ksikk <hgs0219@gachon.ac.kr>
2022-08-04 16:49:13 +0900

    commit by 'v' / 22.08.04./04:19

--- app/src/main/java/com/gachonumc/threejeon/HomeActivity.kt
+++ app/src/main/java/com/gachonumc/threejeon/HomeActivity.kt
@@ -1,13 +1,21 @@
 package com.gachonumc.threejeon
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gachonumc.threejeon.databinding.ActivityHomeBinding
+import com.gachonumc.threejeon.store.StoreActivity
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStore.setOnClickListener {
+            startActivity(Intent(this, StoreActivity::class.java))
+        }
     }
 }
-
-
