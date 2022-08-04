2e04ea439d54cb3fa50fc8ead73450278330a445
ksikk <hgs0219@gachon.ac.kr>
2022-08-04 16:20:06 +0900

    commit by 'v' / 22.08.04./04:19

--- app/src/main/java/com/gachonumc/threejeon/SignupActivity.kt
+++ app/src/main/java/com/gachonumc/threejeon/SignupActivity.kt
@@ -1,11 +1,20 @@
 package com.gachonumc.threejeon
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gachonumc.threejeon.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.successAccount.setOnClickListener {
+            startActivity(Intent(this, SuccessAccountActivity::class.java))
+        }
     }
 }
