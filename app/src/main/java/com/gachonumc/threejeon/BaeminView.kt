7f310d9a34fa87a3de7ca47f165e7a17399be17e
smpark0213 <smpark0213@naver.com>
2022-08-04 17:03:46 +0900

    feat server(baeminstoremenu)

--- app/src/main/java/com/gachonumc/threejeon/BaeminView.kt
+++ app/src/main/java/com/gachonumc/threejeon/BaeminView.kt
@@ -4,3 +4,8 @@ package com.gachonumc.threejeon
         fun baeminStoreListSuccess(result: Array<BaeminStoreList>)
         fun baeminStoreListFailure(code : Int, message: String)
     }
+
+interface BaeminStoreMenuView{
+    fun baeminStoreMenuSuccess(result: Array<BaeminStoreMenuList>)
+    fun baeminStoreMenuFailure(code : Int, message: String)
+}
