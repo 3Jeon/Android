fee6bc7ba7a49347ee4323eac43cccb05c2620e6
ksikk <hgs0219@gachon.ac.kr>
2022-08-17 18:52:06 +0900

    commit by 'v' / 22.08.17./18:51

--- app/src/main/java/com/gachonumc/threejeon/StoreMenuActivity.kt
+++ app/src/main/java/com/gachonumc/threejeon/StoreMenuActivity.kt
@@ -2,13 +2,24 @@ package com.gachonumc.threejeon
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gachonumc.threejeon.Platform.Baemin.BaeminStoreList
 import com.gachonumc.threejeon.databinding.ActivityStoreMenuBinding
+import com.gachonumc.threejeon.databinding.FragmentStoreBinding
+import com.gachonumc.threejeon.store.StoreAdapter
 import com.gachonumc.threejeon.store.StoreFragment
 
 class StoreMenuActivity: AppCompatActivity() {
 
     lateinit var binding: ActivityStoreMenuBinding
+
+    private var baeminStoreArray = ArrayList<BaeminStoreList>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +34,12 @@ class StoreMenuActivity: AppCompatActivity() {
             startActivity(Intent(this, HomeActivity::class.java))
         }
 
-        supportFragmentManager.beginTransaction().replace(R.id.store_menu_fl, StoreFragment()).commit()
+        binding.storeMenuRv.adapter = StoreAdapter(baeminStoreArray)
 
+        //recyclerview 에 layoutManager 설정
+        binding.storeMenuRv.layoutManager = LinearLayoutManager(this)
+
+        //supportFragmentManager.beginTransaction().replace(androidx.fragment.R.id.fragment_container_view_tag, StoreFragment()).commit()
     }
 
 }
